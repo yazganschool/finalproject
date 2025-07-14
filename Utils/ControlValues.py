46dd4dca8c56c7864f2a9836eb0012586dda42e4
yazganschool <sy3297@columbia.edu>
2025-07-14 09:13:50 -0400

    logistic regression model

---
+++ Utils/ControlValues.py
@@ -0,0 +1,7 @@
+# Values that stay the same for all models to get a fair test
+class Controls:
+    test_size = 0.2
+    random_state = 42
+    n_estimators = 100
+    max_depth = 7
+    class_weight = 'balanced'
